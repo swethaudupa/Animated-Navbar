28b0b2b27cd8d31e2b2c9042c1f501f6c342d8f3
swethaudupa <swethaudupag@gmail.com>
2019-11-06 03:13:23 +0530

    animated navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
     </div>
   );
+};
+
+const headerItems = ["HOME", "ABOUT ME", "PROJECTS", "CONTACT"];
+
+// the static letter style
+// we add the animation delay to this object for each letter below
+const LETTER_STYLE = {
+  animationName: "bob",
+  animationDuration: "1s",
+  animationTimingFunction: "ease-in-out",
+  animationIterationCount: "infinite"
+};
+
+// this value is multiplied by each letter's index
+// larger values mean more 'cycles' of animation per word
+const ANIMATION_DELAY_SEPARATION = 0.1;
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoverWord: null
+    };
+  }
+  mouseEnterHandler = word => {
+    this.setState({ hoverWord: word });
+  };
+  mouseLeaveHandler = () => {
+    this.setState({ hoverWord: null });
+  };
+  render() {
+    const { hoverWord } = this.state;
+    return (
+      <div className="header-nav">
+        {headerItems.map(item => {
+          return (
+            <div
+              key={item}
+              className="header-nav-item"
+              onMouseEnter={() => this.mouseEnterHandler(item)}
+              onMouseLeave={() => this.mouseLeaveHandler()}
+            >
+              {item.split("").map((letter, i) => {
+                if (letter.trim()) {
+                  let letterStyle = {};
+                  if (hoverWord === item) {
+                    letterStyle = {
+                      ...LETTER_STYLE,
+                      animationDelay: `${ANIMATION_DELAY_SEPARATION * i}s`
+                    };
+                  }
+                  return (
+                    <div
+                      key={letter + i}
+                      style={letterStyle}
+                      className="header-nav-item-letter"
+                    >
+                      {letter}
+                    </div>
+                  );
+                }
+                return letter;
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
